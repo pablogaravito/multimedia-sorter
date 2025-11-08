ead0a9a620a3f4cbcdf70cbf22b8e45061fca2f0
Pablo <pablo.garavito@gmail.com>
2025-11-07 23:42:27 -0500

    added springboot start up project

---
+++ multimedia-sorter/src/main/java/com/pablogb/multimedia_sorter/MultimediaSorterApplication.java
@@ -0,0 +1,13 @@
+package com.pablogb.multimedia_sorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimediaSorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimediaSorterApplication.class, args);
+	}
+
+}
---
+++ multimedia-sorter/src/test/java/com/pablogb/multimedia_sorter/MultimediaSorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablogb.multimedia_sorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediaSorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
